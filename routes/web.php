1e2594f057d082b98792816e258387ff6bb31b45
pradnyashil012 <56130908+pradnyashil012@users.noreply.github.com>
2021-05-23 18:28:53 +0530

    Shop, Cart & Checkout Pages Made

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use App\Http\Livewire\HomeComponent;
+use App\Http\Livewire\ShopComponent;
+use App\Http\Livewire\CartComponent;
+use App\Http\Livewire\CheckoutComponent;
+
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,3 +24,9 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/', HomeComponent::class);
+
+Route::get('/shop', ShopComponent::class);
+
+Route::get('/cart', CartComponent::class);
+
+Route::get('/checkout', CheckoutComponent::class);
